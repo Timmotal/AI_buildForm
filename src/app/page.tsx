7561a42c81adcf9298b74b85a8b4aa4e55c69a84
Timmotal <timmortal44@yahoo.com>
2025-01-17 22:59:19 +0100

    Add LandingPage component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,2 +1,7 @@
 import Header from "@/components/ui/header";
 import { SessionProvider } from 'next-auth/react';
+import LandingPage from './landing-page';
+
+export default function Home() {
+  
+}
