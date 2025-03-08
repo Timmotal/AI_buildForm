057f4d5966b06ece54fe29a719508a107818bc0f
Timmotal <timmortal44@yahoo.com>
2025-03-08 20:21:08 +0000

    and primary forgroound

--- src/app/components/progressBar.tsx
+++ src/app/components/progressBar.tsx
@@ -7,7 +7,7 @@ type Props = {
 const ProgressBar = (props: Props) => {
   return (
     <div className='w-full bg-gray-200 rounded-full h-2.5'>
-     
+      <div className='bg-primary h-2.5 rounded-md' style={{ width: `${props.value}%` }}></div>
     </div>
   )
 }
