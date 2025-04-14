382e7df09025d3c660acacd7af40a53f8d3ff324
Timmotal <timmortal44@yahoo.com>
2025-04-14 22:48:06 +0100

    Fix minor inconsistencies in className handling

--- src/app/components/ui/card.tsx
+++ src/app/components/ui/card.tsx
@@ -38,5 +38,9 @@ const CardDescription = React.forwardRef<
   HTMLParagraphElement,
   React.HTMLAttributes<HTMLParagraphElement>
 >(({ className, ...props }, ref) => (
-
+  <p
+    ref={ref}
+    className={cn("text-sm text-muted-foreground", className)}
+    {...props}
+  />
 ))
