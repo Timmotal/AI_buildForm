d76404bb1a23ca5e61215775dca02f206f6c624b
Timmotal <timmortal44@yahoo.com>
2025-04-26 23:37:12 +0100

    Improve performance by reducing unnecessary renders

--- src/app/components/ui/card.tsx
+++ src/app/components/ui/card.tsx
@@ -51,5 +51,5 @@ const CardContent = React.forwardRef<
   HTMLDivElement,
   React.HTMLAttributes<HTMLDivElement>
 >(({ className, ...props }, ref) => (
- 
+  <div ref={ref} className={cn("p-6 pt-0", className)} {...props} />
 ))
