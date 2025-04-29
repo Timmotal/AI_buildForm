6c9ae5c4aa98d8a1db545ceffaa92a8daee9a73d
Timmotal <timmortal44@yahoo.com>
2025-04-29 22:14:01 +0100

    Add default spacing values for better layout consistency

--- src/app/components/ui/card.tsx
+++ src/app/components/ui/card.tsx
@@ -53,3 +53,13 @@ const CardContent = React.forwardRef<
 >(({ className, ...props }, ref) => (
   <div ref={ref} className={cn("p-6 pt-0", className)} {...props} />
 ))
+
+CardContent.displayName = "CardContent"
+
+const CardFooter = React.forwardRef<
+  HTMLDivElement,
+  React.HTMLAttributes<HTMLDivElement>
+>(({ className, ...props }, ref) => (
+
+))
+
