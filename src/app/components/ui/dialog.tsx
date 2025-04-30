3114968e299e72b433ffcc7bd24c5f32eb6a0c28
Timmotal <timmortal44@yahoo.com>
2025-04-30 20:07:26 +0100

    add dialog close functionality with icon button

--- src/app/components/ui/dialog.tsx
+++ src/app/components/ui/dialog.tsx
@@ -14,3 +14,9 @@ const DialogPortal = DialogPrimitive.Portal
 
 const DialogClose = DialogPrimitive.Close
 
+const DialogOverlay = React.forwardRef<
+  React.ElementRef<typeof DialogPrimitive.Overlay>,
+  React.ComponentPropsWithoutRef<typeof DialogPrimitive.Overlay>
+>(({ className, ...props }, ref) => (
+
+))
