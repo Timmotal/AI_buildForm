2a1b358d8261b58219c13dda1643dc6f61e2b5fa
Timmotal <timmortal44@yahoo.com>
2025-05-06 19:16:05 +0100

    implement dialog content with complex animation transitions

--- src/app/components/ui/dialog.tsx
+++ src/app/components/ui/dialog.tsx
@@ -28,3 +28,10 @@ const DialogOverlay = React.forwardRef<
   />
 ))
 DialogOverlay.displayName = DialogPrimitive.Overlay.displayName
+
+const DialogContent = React.forwardRef<
+  React.ElementRef<typeof DialogPrimitive.Content>,
+  React.ComponentPropsWithoutRef<typeof DialogPrimitive.Content>
+>(({ className, children, ...props }, ref) => (
+
+ 
