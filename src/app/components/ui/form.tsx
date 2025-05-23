5500ea9a96629cd32e0bd3c074040fdedc97f4e6
Timmotal <timmortal44@yahoo.com>
2025-05-23 20:19:50 +0100

    Link FormLabel to corresponding form field by ID

--- src/app/components/ui/form.tsx
+++ src/app/components/ui/form.tsx
@@ -18,10 +18,13 @@ const Form = FormProvider
 
 type FormFieldContextValue<
   TFieldValues extends FieldValues = FieldValues,
- 
+  TName extends FieldPath<TFieldValues> = FieldPath<TFieldValues>
 > = {
   name: TName
 }
 
+const FormFieldContext = React.createContext<FormFieldContextValue>(
+  {} as FormFieldContextValue
+)
 
 
