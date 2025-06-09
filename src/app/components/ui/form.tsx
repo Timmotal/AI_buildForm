ba335fa08204a9207b5bf06346d27eca092ba5fd
Timmotal <timmortal44@yahoo.com>
2025-06-09 22:51:07 +0100

    Implement FormDescription for accessibility support

--- src/app/components/ui/form.tsx
+++ src/app/components/ui/form.tsx
@@ -31,4 +31,11 @@ const FormField = <
   TFieldValues extends FieldValues = FieldValues,
   TName extends FieldPath<TFieldValues> = FieldPath<TFieldValues>
 >({
-
+  ...props
+}: ControllerProps<TFieldValues, TName>) => {
+  return (
+    <FormFieldContext.Provider value={{ name: props.name }}>
+      <Controller {...props} />
+    </FormFieldContext.Provider>
+  )
+}
