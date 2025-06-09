749600e62ea826b709e5f7a724a5f1bec096629e
Timmotal <timmortal44@yahoo.com>
2025-06-09 22:52:37 +0100

    Conditionally render error message in FormMessage

--- src/app/components/ui/form.tsx
+++ src/app/components/ui/form.tsx
@@ -43,7 +43,7 @@ const FormField = <
 const useFormField = () => {
   const fieldContext = React.useContext(FormFieldContext)
   const itemContext = React.useContext(FormItemContext)
-
+  const { getFieldState, formState } = useFormContext()
 
 
 
