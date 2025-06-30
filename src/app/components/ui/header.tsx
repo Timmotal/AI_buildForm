6e03cf7691253337396ae36c3b4e14b04ff416d3
Timmotal <timmortal44@yahoo.com>
2025-06-30 21:49:39 +0100

    Make user image round and size it to 32x32

--- src/app/components/ui/header.tsx
+++ src/app/components/ui/header.tsx
@@ -20,7 +20,7 @@ const Header = async (props: Props) => {
   const session = await auth();
 
   return (
-    <header className=''>
+    <header className='border bottom-1'><nav className='bg-white border-gray-200 px-4 py-2.5'>
       <div className='flex flex-wrap justify-between items-center mx-auto max-w-screen-xl'><h1>AI Form Builder</h1><div>
         {
           session?.user ? (
